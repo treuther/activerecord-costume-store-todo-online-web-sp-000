aef404c35e2016c45a6ec25b2f2fe06aac1fbec8
Tracy Reuther <tracyreuther@gmail.com>
2019-08-20 23:23:33 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -8,7 +8,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.string :name
       t.float :price
       t.string :image_url
-      t.string :size 
+      t.string :size
   end
 # Define a change method in which to do the migration
 # In this change method, create columns with the correct names and
