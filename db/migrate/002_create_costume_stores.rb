17970ec2d897051c8b096bf977947280915e72a0
Tracy Reuther <tracyreuther@gmail.com>
2019-08-20 23:38:43 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
+      t.integer :costume_inventory
 
     end
   end
